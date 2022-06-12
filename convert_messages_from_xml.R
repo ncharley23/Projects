216f36ca88027e79da7f2662742e3ecf7287803c
Nicholas Charley <ncharley@alumni.uncc.edu>
2022-06-12 15:37:55 -0400

    adding who sent the messages

--- convert_messages_from_xml.R
+++ convert_messages_from_xml.R
@@ -12,5 +12,10 @@ df <- data.frame(
   person = messages %>% xml_attr("contact_name"),
   date_time = messages %>% xml_attr("readable_date"),
   body = messages %>% xml_attr("body"),
+  who_sent_it = messages %>% xml_attr("type"),
+  
   number = messages %>% xml_attr("address")
 )
+
+df<-df %>% mutate(who_sent_it = case_when(who_sent_it=="2" ~ "Me", 
+                                    who_sent_it == "1" ~ person,  TRUE ~ who_sent_it))
