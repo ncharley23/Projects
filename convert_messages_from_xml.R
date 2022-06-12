8706bab214ab129e5826f79fed65165fd2289ad0
Nicholas Charley <ncharley@alumni.uncc.edu>
2022-06-12 17:19:56 -0400

    Update convert_messages_from_xml.R

--- convert_messages_from_xml.R
+++ convert_messages_from_xml.R
@@ -13,18 +13,22 @@ df <- data.frame(
   date_time = messages %>% xml_attr("readable_date"),
   body = messages %>% xml_attr("body"),
   who_sent_it = messages %>% xml_attr("type"),
-  number = messages %>% xml_attr("address")
+  who_is_being_spoken_to = messages %>% xml_attr("address")
 )%>%
-# %>% mutate(who_sent_it = case_when(who_sent_it=="2" ~ "Me", 
-#                                     who_sent_it == "1" ~ person))
 
 #identify the sender
 mutate(who_sent_it= case_when(who_sent_it=="1" ~ person, 
                               who_sent_it=="2"~ "Me" ))
 
-#case did not work with other cases
-df<-df%>%mutate(who_sent_it= case_when(str_detect(who_sent_it, "Unknown")==TRUE ~ number,
+#case did not work with other cases but still identifying send
+df<-df%>%mutate(who_sent_it= case_when(str_detect(who_sent_it, "Unknown")==TRUE ~ who_is_being_spoken_to,
                 TRUE~who_sent_it))
 
-my_messages<-df%>%select(who_sent_it,body, date_time)
+df<-df%>%mutate(who_is_being_spoken_to = gsub("\\+","",who_is_being_spoken_to))
 
+
+#select only what you need to see
+my_messages<-df%>%select(who_sent_it,body, date_time,who_is_being_spoken_to)
+
+#mimicking a thread
+my_messages<-my_messages%>%arrange(who_is_being_spoken_to,date_time)
