8ec86151bbe6a67fe27b7700606cae1396b1146f
Nicholas Charley <ncharley@alumni.uncc.edu>
2022-06-12 15:06:11 -0400

    removing library and adding comments

--- convert_messages_from_xml.R
+++ convert_messages_from_xml.R
@@ -1,17 +1,16 @@
-
-library(xmlconvert)
 library(xml2)
-library(xml)
-library(XML)
-library(tidyverse)
 library(dplyr)
 library(rvest)
 
-rows <- read_xml('C:/Users/Owner/Downloads/sms-20190815211620.xml',options="HUGE") %>% 
-  xml_nodes('sms')
+#once you've downloaded your file to your computer, replace my file path below with YOUR file path
+messages <- read_xml('C:/Users/Owner/Downloads/sms-20190815211620.xml',options="HUGE") %>% 
+  html_elements('sms')
+
+#the rest will take care of itself
+#grabbing your sms info from the XML file
 df <- data.frame(
-  person = rows %>% xml_attr("contact_name"),
-  date_time = rows %>% xml_attr("readable_date"),
-  body = rows %>% xml_attr("body"),
-  number = rows %>% xml_attr("address")
+  person = messages %>% xml_attr("contact_name"),
+  date_time = messages %>% xml_attr("readable_date"),
+  body = messages %>% xml_attr("body"),
+  number = messages %>% xml_attr("address")
 )
