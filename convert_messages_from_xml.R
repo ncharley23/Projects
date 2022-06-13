8f4ea8ca60c82d882f40e8720ef43bfca3d2f992
Nicholas Charley <ncharley@alumni.uncc.edu>
2022-06-13 04:14:07 -0400

    create actual message thread

--- convert_messages_from_xml.R
+++ convert_messages_from_xml.R
@@ -13,22 +13,27 @@ df <- data.frame(
   date_time = messages %>% xml_attr("readable_date"),
   body = messages %>% xml_attr("body"),
   who_sent_it = messages %>% xml_attr("type"),
-  who_is_being_spoken_to = messages %>% xml_attr("address")
+  message_thread = messages %>% xml_attr("address")
 )%>%
 
 #identify the sender
 mutate(who_sent_it= case_when(who_sent_it=="1" ~ person, 
                               who_sent_it=="2"~ "Me" ))
 
-#case did not work with other cases but still identifying send
-df<-df%>%mutate(who_sent_it= case_when(str_detect(who_sent_it, "Unknown")==TRUE ~ who_is_being_spoken_to,
+#showing you your unsaved numbers
+df<-df%>%mutate(who_sent_it= case_when(str_detect(who_sent_it, "Unknown")==TRUE ~ message_thread,
                 TRUE~who_sent_it))
 
-df<-df%>%mutate(who_is_being_spoken_to = gsub("\\+","",who_is_being_spoken_to))
+#if the number is saved as a contact, it will connect you and the person as a thread of messages
+df<-df%>%mutate(message_thread = case_when((str_detect(df$who_sent_it, '^[^+]+$')|is.na(df$who_sent_it))==TRUE& str_detect(person, fixed("("))!=TRUE~ person,
+                                                   TRUE~message_thread))
+
+#getting rid of your plus signs to group numbers that may be the same(i.e, +1234567890,1234567890)
+df<-df%>%mutate(message_thread = gsub("\\+","",message_thread))
 
 
 #select only what you need to see
-my_messages<-df%>%select(who_sent_it,body, date_time,who_is_being_spoken_to)
+my_messages<-df%>%select(who_sent_it,body, date_time,message_thread)
 
 #mimicking an actual thread of conversation
-my_messages<-my_messages%>%arrange(who_is_being_spoken_to,date_time)
+my_messages<-my_messages%>%arrange(message_thread,date_time)
